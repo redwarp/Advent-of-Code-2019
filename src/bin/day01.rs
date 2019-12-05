21cfd2598c10bd5cde909f6b23e76c3a0f9dcd2b
Benoit Vermont <bvermont@yelp.com>
2019-12-05 13:15:10 +0100

    Move line read to another file

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,16 +1,4 @@
-use std::{
-    fs::File,
-    io::{prelude::*, BufReader},
-    path::Path,
-};
-
-fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
-    let file = File::open(filename).expect("no such file");
-    let buf = BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect()
-}
+use adventofcode::files;
 
 fn fuel_for_mass(mass: i32) -> i32 {
     (mass - (mass % 3)) / 3 - 2
@@ -32,7 +20,7 @@ fn fuel_for_mass_with_extra(mass: i32) -> i32 {
 
 fn main() {
     println!("Mass: {}", fuel_for_mass(100756));
-    let lines = lines_from_file("inputs/day01.txt");
+    let lines = files::lines_from_file("inputs/day01.txt");
     let fuel: i32 = lines
         .iter()
         .map(|x| fuel_for_mass(x.parse().unwrap()))
